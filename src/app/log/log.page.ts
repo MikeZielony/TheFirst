23060a8578506afa15719eb1d50cd7a492400aec
MikeZielony <mike.zielony@gmail.com>
2024-08-25 15:57:44 +0200

    first code

---
+++ src/app/log/log.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log',
+  templateUrl: './log.page.html',
+  styleUrls: ['./log.page.scss'],
+})
+export class LogPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  log() {
+    alert("Be carefully")
+  }
+}
